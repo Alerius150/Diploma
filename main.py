bec711d4720f2078061ca533c48769b07fb2592f
talgat_moldazhanov <talgat_0508@gmail.com>
2023-08-19 13:12:52 +0600

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 Файл для запуска бота и создания БД, в случае её отсутствия
 """
 
-import handlers
 from database.models import DataBaseModel
 from loader import bot, logger
 
 
 if __name__ == '__main__':
+    # noinspection PyProtectedMember
     DataBaseModel._init_user_tables()
     bot.infinity_polling()
     logger.info('bot start working')
