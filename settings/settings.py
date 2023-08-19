ce0180f369451d6e6bb712cca0f8441f0ef3ba24
talgat_moldazhanov <talgat_0508@gmail.com>
2023-08-19 12:50:20 +0600

    Merge remote-tracking branch 'origin/master'

--- settings/settings.py
+++ settings/settings.py
@@ -6,82 +6,72 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 if not find_dotenv():
-  exit('Файл .env отсутствует')
+    exit('Файл .env отсутствует')
 else:
-  load_dotenv()
+    load_dotenv()
 
 TOKEN = os.environ.get('TOKEN')
 API_KEY = os.environ.get('API_KEY')
 
 
 HEADERS = {
-  'X-RapidAPI-Host': 'hotels4.p.rapidapi.com',
-  'X-RapidAPI-Key': API_KEY
+    'X-RapidAPI-Host': 'hotels4.p.rapidapi.com',
+    'X-RapidAPI-Key': API_KEY
 }
 
 
-URL_SEARCH = 'https://hotels4.p.rapidapi.com/locations/v2/search'
-URL_PROPERTY_LIST = 'https://hotels4.p.rapidapi.com/properties/v2/list'
+URL_SEARCH = 'https://hotels4.p.rapidapi.com/locations/v3/search'
+URL_PROPERTY_LIST = 'https://hotels4.p.rapidapi.com/properties/v3/list'
 URL_PHOTO = 'https://hotels4.p.rapidapi.com/properties/v2/detail'
-URL_HOTEL = 'https://www.hotels.com/ho{}'
+URL_HOTEL = 'https://www.hotels.com/ho{}''
 
 
 QUERY_SEARCH = {
-  'query': 'new_york',
-  'locale': 'en_US',
-  'currency': 'USD'
+    'query': 'new_york',
+    'locale': 'en_US',
+    'currency': 'USD'
 }
-
-QUERY_PROPERTY_LIST = {
-  'currency': 'USD',
-  'eapid': 1,
-  'locale': 'ru_RU',
-  'siteId': 300000001,
-  'destination': {'regionId': '2621'},
-  'checkInDate': {
-    'day': 10,
-    'month': 10,
-    'year': 2023
-  },
-  'checkOutDate': {
-    'day': 15,
-    'month': 10,
-    'year': 2023
-  },
-  'rooms': [{'adults': 1}],
-  'resultsStartingIndex': 0,
-  'resultsSize': 200,
-  'sort': 'PRICE_LOW_TO_HIGH',
-  'filters': {'price': {
-    'max': 11000,
-    'min': 1
-  }}
+QUERY_PROPERTY_LIST = {{
+	"currency": "USD",
+	"eapid": 1,
+	"locale": "en_US",
+	"siteId": 300000001,
+	"destination": { "regionId": "6054439" },
+	"checkInDate": {
+		"day": 10,
+		"month": 10,
+		"year": 2022
+	},
+	"checkOutDate": {
+		"day": 15,
+		"month": 10,
+		"year": 2022
+	},
+	"rooms": [
+		{
+			"adults": 2,
+			"children": [{ "age": 5 }, { "age": 7 }]
+		}
+	],
+	"resultsStartingIndex": 0,
+	"resultsSize": 200,
+	"sort": "PRICE_LOW_TO_HIGH",
+	"filters": { "price": {
+			"max": 150,
+			"min": 100
+		} }
 }
-QUERY_CUSTOM =  {
-  'currency': 'USD',
-  'eapid': 1,
-  'locale': 'ru_RU',
-  'siteId': 300000001,
-  'destination': {'regionId': '2621'},
-  'checkInDate': {
-    'day': 10,
-    'month': 10,
-    'year': 2023
-  },
-  'checkOutDate': {
-    'day': 15,
-    'month': 10,
-    'year': 2023
-  },
-  'rooms': [{'adults': 1}],
-  'resultsStartingIndex': 0,
-  'resultsSize': 200,
-  'sort': 'PRICE_LOW_TO_HIGH',
-  'filters': {'price': {
-    'max': 11000,
-    'min': 1
-  }}
+QUERY_BESTDEAL = {
+    'destinationId': '1506246',
+    'pageNumber': '1',
+    'pageSize': '25',
+    'checkIn': '2020-01-08',
+    'checkOut': '2020-01-15',
+    'adults1': '1',
+    'priceMin': '50',
+    'priceMax': '300',
+    'sortOrder': 'DISTANCE_FROM_LANDMARK',
+    'locale': 'en_US',
+    'currency': 'EUR'
 }
-
-
 QUERY_PHOTO = {'id': '1178275040'}
