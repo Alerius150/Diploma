1b9205f6c3634c1f43803809a8202d1716dc189f
talgat_moldazhanov <talgat_0508@gmail.com>
2023-08-20 12:29:26 +0600

    Merge remote-tracking branch 'origin/master'

--- settings/settings.py
+++ settings/settings.py
@@ -77,3 +77,30 @@ QUERY_BESTDEAL = {
 }
 
 QUERY_PHOTO = {'id': '1178275040'}
+
+
+QUERY_CUSTOM =  {
+  'currency': 'USD',
+  'eapid': 1,
+  'locale': 'ru_RU',
+  'siteId': 300000001,
+  'destination': {'regionId': '2621'},
+  'checkInDate': {
+    'day': 10,
+    'month': 10,
+    'year': 2023
+  },
+  'checkOutDate': {
+    'day': 15,
+    'month': 10,
+    'year': 2023
+  },
+  'rooms': [{'adults': 1}],
+  'resultsStartingIndex': 0,
+  'resultsSize': 200,
+  'sort': 'PRICE_LOW_TO_HIGH',
+  'filters': {'price': {
+    'max': 11000,
+    'min': 1
+  }}
+}
